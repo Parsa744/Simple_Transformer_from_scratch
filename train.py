f13eaae25ddeca1af3b4a6dcd40d42c16c48c4ac
Parsa Hr <51270701+Parsa744@users.noreply.github.com>
2025-11-05 12:04:51 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import numpy as np
+import torch.nn.functional as F
 
-from dataloder import load_data , vec2eng , vec2de
+from dataloder import load_data , vec2eng , vec2de,add_start_token
 from models import SimpleTransformer
 import torch
 import matplotlib.pyplot as plt
@@ -22,26 +23,34 @@ def main():
         de_sentence = row[0]  # German sentence vectors (list of vectors)
         eng_sentence = row[1]  # English sentence vectors (list of vectors)
 
+        eng_sentence_with_start = add_start_token(eng_sentence)
         # Convert to tensors
+
         de_tensor = torch.tensor(de_sentence, dtype=torch.float32).unsqueeze(0)  # Add batch dimension
         eng_tensor = torch.tensor(eng_sentence, dtype=torch.float32).unsqueeze(0)  # Add batch dimension
+        eng_tensor_with_start = torch.tensor(eng_sentence_with_start, dtype=torch.float32).unsqueeze(0)  # Add batch dimension
 
 
         # Forward pass
-        pred = transformer.forward(de_tensor, eng_tensor)
+        pred = transformer.forward(de_tensor, eng_tensor_with_start)
         pred = pred.squeeze(0)  # Remove batch dimension
-        loss = torch.nn.MSELoss()
+        #loss = torch.nn.MSELoss()
         target = eng_tensor
-        output = loss(pred, target)
+        #output = loss(pred, target)
+        cos_sim = F.cosine_similarity(pred, target, dim=1)
+        output = 1 - cos_sim.mean()
         print('output loss:', output.item())
         optim.zero_grad()
         output.backward()
         optim.step()
         losses.append(output.item())
         counter += 1
-        if counter % 100 == 0:
-            # TODO: translate back to words and print
-            print(f'Iteration {counter}, Loss: {output.item()}')
+        if counter % 10 == 0:
+            #print(f'Iteration {counter}, Loss: {output.item()}')
+            #print('shape of pred:', np.shape(pred))
+            for vec in pred:
+                print(np.shape(vec))
+                print(vec2eng(vec))
 
     # save the model
     torch.save(transformer.state_dict(), 'transformer_model.pth')
